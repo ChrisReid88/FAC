eb046d32d2214c010f5c86964fe7f7810f0305b0
LAPTOP-A68GSSV9\reidm <reidymarc@googlemail.com>
2018-04-10 13:43:17 +0100

    Updated GUI and some logic to handle bad logins/verification [Marc]

--- gui.py
+++ gui.py
@@ -1,25 +1,41 @@
 import kivy
 from kivy.app import App
+from kivy.config import Config
+from kivy.factory import Factory
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 from kivy.properties import ObjectProperty
-from kivy.config import Config
 from kivy.network.urlrequest import UrlRequest
 import json
+
 kivy.require('1.10.0')
 
-# Makes window unable to be fullscreen
+# Makes window unable to be fullscreen and sets the size
 Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '850')
+Config.set('graphics', 'height', '600')
+Config.set('input', 'mouse', 'mouse, multitouch_on_demand')
 
 
 class LoginPage(Screen):
     inputStringUsername = ObjectProperty()
     inputStringPassword = ObjectProperty()
 
-    def output_username(self):                  # Handle the username from login screen
+    def output_username(self):  # Handle the username from login screen
         print(self.inputStringUsername.text)
 
-    def output_password(self):                  # Handle the password from login screen
+    def output_password(self):  # Handle the password from login screen
         print(self.inputStringPassword.text)
+
+    def username_and_password_check(self, username, password):  # if statement to handle good and bad login attempts
+        if username == " " and password == " ":
+            self.manager.current = 'menu'
+            self.clear_details()
+        else:
+            Factory.BadDetailsPopup().open()
+
+    def clear_details(self):
+        self.inputStringUsername.text = ""
+        self.inputStringPassword.text = ""
 
 
 class MainMenuPage(Screen):
@@ -27,29 +43,56 @@ class MainMenuPage(Screen):
 
 
 class AddFirearmPage(Screen):
-    inputStringFirstName: ObjectProperty()
-    inputStringSurName: ObjectProperty()
+    inputStringLicenceNumber: ObjectProperty()  # THESE VARIABLE NAMES HAVE CHANGED
+    inputStringTransactionNumber: ObjectProperty()
+    inputStringFirearmModel: ObjectProperty()
     inputStringSerialNumber: ObjectProperty()
+    inputStringStoreID: ObjectProperty()
+    inputStringEmployeeID: ObjectProperty()
 
-    def output_form_details(self):
-        print(self.inputStringFirstName.text)
-        print(self.inputStringSurName.text)
-        print(self.inputStringSerialNumber.text)
+    verified = False
 
-    def add_txn(self):
-        pass
+    def output_form_details(self, username, password):  # NEED THIS TO CHECK A LIST WITH STORED LOGIN DETAILS
+        if username == " " and password == " ":
+            self.verified = True
+        else:
+            Factory.BadDetailsPopup().open()
+
+    def add_to_the_blockchain(self):  # ADD CODE TO ADD INPUT INFO TO THE BLOCK IN THIS FUNCTION
+        if self.verified is True:
+            print("Added to the block")
+        else:
+            Factory.NotVerifiedPopup().open()
+
+        def add_txn(self):
+            pass
+
 
 class ViewBlockchainPage(Screen):
-    outputStringDetails:  ObjectProperty()
-    test_string = "First Name: " + "" + "\nSurname: " + "" + "\nSerial Number: " + "" + ""
+    outputStringBlockID: ObjectProperty()
+    outputStringLicenceNumber: ObjectProperty()
+    outputStringTransactionNumber: ObjectProperty()
+    outputStringFirearmModel: ObjectProperty()
+    outputStringSerialNumber: ObjectProperty()
+    outputStringStoreID: ObjectProperty()
+    outputStringEmployeeID: ObjectProperty()
+
+    test_blockID = "123"  # TEST VALUES
+    test_licenceNumber = "00000001"  # TEST VALUES
+    test_transactionNumber = "748575"  # TEST VALUES
+
     def output_to_label(self):
-            self.outputStringDetails.text = self.get_chain()
+        self.outputStringBlockID.text = self.test_blockID
+        self.outputStringLicenceNumber.text = self.test_licenceNumber
+        self.outputStringTransactionNumber.text = self.test_transactionNumber
+        # self.outputStringEmployeeID.text = self.get_chain()  # THIS IS FROM YOUR CODE AND CAUSES A CRASH
+
 
 class Manager(ScreenManager):
-    screen_one = ObjectProperty(None)       # Login Page
-    screen_two = ObjectProperty(None)       # Main Menu Page
-    screen_three = ObjectProperty(None)     # Add Firearm Page
-    screen_four = ObjectProperty(None)      # View Blockchain Page
+    screen_one = ObjectProperty(None)  # Login Page
+    screen_two = ObjectProperty(None)  # Main Menu Page
+    screen_three = ObjectProperty(None)  # Add Firearm Page
+    screen_four = ObjectProperty(None)  # View Blockchain Page
 
 
 class GroupProjectApp(App):
@@ -61,4 +104,3 @@ class GroupProjectApp(App):
 
 if __name__ == '__main__':
     GroupProjectApp().run()
-
